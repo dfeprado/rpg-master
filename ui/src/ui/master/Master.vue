2f970521b21ed9343e38f45e2685a61917820eb4
Daniel Prado <dfeprado@gmail.com>
2024-11-09 02:10:50 -0300

    Make the default interface

--- ui/src/ui/master/Master.vue
+++ ui/src/ui/master/Master.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Hello, Master!</h1>
+  <div class="flex h-full">
+    <nav class="nav top">Navigator</nav>
+    <section class="flex-grow nav scrollable-y">
+      <p v-for="n in 1">View {{ n }}</p>
+    </section>
+    <footer class="nav bottom">footer</footer>
+  </div>
 </template>
+
+<style lang="css" scoped>
+.nav {
+  padding: 0.75rem 0.5rem;
+}
+
+.nav.top {
+  border-bottom: 1px solid black;
+}
+
+.nav.bottom {
+  border-top: 1px solid black;
+}
+
+.scrollable-y {
+  overflow-y: scroll;
+}
+</style>
